c206bebc79adcd6782dcdaa93e04af3f40e1960f
kyanpepper <kyankks1@gmail.com>
2024-10-09 20:05:24 -0700

    Buff overflow complete

--- Buffer Overflow/main.c
+++ Buffer Overflow/main.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-void vuln(char *input) {
-    char buffer[100];
-    strcpy(buffer, input);
-}
-
-void getPassword() {
-    char password[100];
-    printf("Enter the password: ");
-    gets(password);
-    printf("You entered");
-    printf(password);
+void vulnFunction(char *input) {
+    char buffer[5]; //This is our buffer
+    strcpy(buffer, input);// This is a buffer overflow
 }
 
 void debug() {
     printf("Debugging mode activated\n");
-    system("/bin/sh");
+    system("/bin/sh"); // This is a backdoor
 }
 
 int main() {
-    
+    char input[100]; //This our input buffer
+    printf("Enter your name: "); 
+    scanf(&input); // This is a could be a buffer overflow
+    vulnFunction(input);
+    return 0;
 }
